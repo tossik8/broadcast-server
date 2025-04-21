c890eb17fcbb102a395bc2cd489d7114fd3710a0
Nikita Toropov <toropov.nikita2004@gmail.com>
2025-04-21 18:42:25 +0200

    Implement creation of groups

--- src/server.py
+++ src/server.py
@@ -6,13 +6,13 @@ from threading import Thread, Lock
 HOST = "0.0.0.0"
 
 logger = get_server_logger()
-connections: list[socket.socket] = []
+connections_by_group: dict[str, set[socket.socket]] = { "general": set() }
 lock = Lock()
 
 
-def _broadcast(data: bytes, sender) -> None:
+def _broadcast(group: str, data: str, sender) -> None:
     with lock:
-        copy = connections.copy()
+        copy = connections_by_group[group].copy()
     for conn in copy:
         try:
             if conn.getpeername() == sender:
@@ -20,9 +20,9 @@ def _broadcast(data: bytes, sender) -> None:
         except socket.error:
             logger.debug("Could not get peer name. Connection had been terminated before.")
             continue
-        message = f"From {sender}: {data.decode()}"
+        message = f"From {sender}: {data}".encode()
         try:
-            conn.sendall(message.encode())
+            conn.sendall(message)
         except socket.error:
             logger.warning("Could not broadcast.")
 
@@ -30,14 +30,50 @@ def _broadcast(data: bytes, sender) -> None:
 def _shutdown_server() -> None:
     logger.info("Shutting down server...")
     with lock:
-        for conn in connections:
-            logger.info(f"Closing connection with {conn.getpeername()}...")
-            conn.shutdown(socket.SHUT_RDWR)
-            conn.close()
-        connections.clear()
+        for connections in connections_by_group.values():
+            for conn in connections:
+                logger.info(f"Closing connection with {conn.getpeername()}...")
+                conn.shutdown(socket.SHUT_RDWR)
+                conn.close()
+    logger.info("Server shut down.")
+
+
+def _change_groups(group: str, data: str, conn: socket.socket) -> str:
+    new_group = data[7:]
+    if len(new_group) == 0:
+        raise ValueError("Group name cannot be empty.")
+    if new_group == group:
+        raise ValueError("Group name must be different from the currently active group.")
+    with lock:
+        connections_by_group[group].remove(conn)
+        if new_group in connections_by_group:
+            connections_by_group[new_group].add(conn)
+        else:
+            connections_by_group[new_group] = set([conn])
+    return new_group
+
+
+def _handle_group_change(group: str, data: str, conn: socket.socket, address) -> str:
+    try:
+        group = _change_groups(group, data, conn)
+    except ValueError as e:
+        logger.warning(e)
+        try:
+            conn.sendall(f"From {conn.getsockname()}: {str(e)}".encode())
+        except socket.error:
+            logger.warning(f"Could not send the reason for group change failure to {address}.")
+    else:
+        try:
+            conn.sendall(f"From {conn.getsockname()}: You switched to group '{group}'".encode())
+        except socket.error:
+            logger.warning(f"Could not send group change confirmation to {address}.")
+        else:
+            logger.info(f"{address} switched to group '{group}'.")
+    return group
 
 
 def _receive_messages(conn: socket.socket, address) -> None:
+    group = "general"
     with conn:
         while True:
             try:
@@ -47,18 +83,19 @@ def _receive_messages(conn: socket.socket, address) -> None:
                 break
             if not data:
                 break
-            _broadcast(data, address)
-        try:
-            with lock:
-                connections.remove(conn)
-        except ValueError:
-            logger.debug(f"{address} had been removed from connections list before.")
+            data = data.decode()
+            if data.startswith("GROUP: "):
+                group = _handle_group_change(group, data, conn, address)
+            else:
+                _broadcast(group, data, address)
+        with lock:
+            connections_by_group[group].remove(conn)
     logger.info(f"Connection closed with {address}.")
 
 
 def start_server(args: argparse.Namespace) -> None:
+    logger.info("Starting server...")
     with socket.socket() as server:
-        logger.info("Starting server...")
         try:
             server.bind((HOST, args.port))
         except OSError:
@@ -71,10 +108,9 @@ def start_server(args: argparse.Namespace) -> None:
                 conn, address = server.accept()
                 thread = Thread(target=_receive_messages, args=[conn, address], name=str(address))
                 with lock:
-                    connections.append(conn)
+                    connections_by_group["general"].add(conn)
                 thread.start()
                 logger.info(f"Connected with {address}.")
         except KeyboardInterrupt:
             _shutdown_server()
-    logger.info("Server shut down.")
 
